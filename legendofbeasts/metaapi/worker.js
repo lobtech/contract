84ec3b3a2b62e45c97f42080258820219b07c723
axvn <axvn@hotmail.com>
2022-01-13 09:48:52 +0100

    fix syntax error

--- legendofbeasts/metaapi/worker.js
+++ legendofbeasts/metaapi/worker.js
@@ -1,6 +1,6 @@
-const {ethers} = require('ethers');
-const HatchingEggArtifact require("contracts/HatchingEgg.json");
-const {Dragon} = require('./db');
+const { ethers } = require('ethers');
+const HatchingEggArtifact = require("contracts/HatchingEgg.json");
+const { Dragon } = require('./db');
 const contractAddress = require('contract-address');
 
 const PROVIDER_URL = "https://api.avax-test.network/ext/bc/C/rpc"; // process.env.PROVIDER_URL
@@ -15,28 +15,28 @@ function main() {
 
   hegg.on('DragonBrokeOut', (to, optionId, tokenId) => {
     let dragon = getRandomDragonForOption(optionId);
-    await Dragon.create({name: dragon.name, tokenId: tokenId});
-  };
+    await Dragon.create({ name: dragon.name, tokenId: tokenId });
+  });
 
 }
 
 const dragonTypes = [
   [
-    {name: "灵狐" },
-    {name: "幻鹿" },
-    {name: "水豚" }
+    { name: "灵狐" },
+    { name: "幻鹿" },
+    { name: "水豚" }
   ],
   [
-    {name: "千秋龟" },
-    {name: "孔雀" },
-    {name: "幽冥虎" },
-    {name: "独角兽" },
+    { name: "千秋龟" },
+    { name: "孔雀" },
+    { name: "幽冥虎" },
+    { name: "独角兽" },
   ],
   [
-    {name: "麒麟"},
-    {name: "犬神"},
-    {name: "巨犀"},
-    {name: "兔子"},
+    { name: "麒麟" },
+    { name: "犬神" },
+    { name: "巨犀" },
+    { name: "兔子" },
   ]
 ]
 
